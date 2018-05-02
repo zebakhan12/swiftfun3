40a45b8f255852aa3248daf908d88f18438897f1
Unknown <zebahashimi@gmail.com>
2018-05-02 16:24:18 -0700

    Pulling information from Core Data

--- ToDo List/ToDoTableViewController.swift
+++ ToDo List/ToDoTableViewController.swift
@@ -9,18 +9,33 @@
 import UIKit
 
 class ToDoTableViewController: UITableViewController {
-
-    var toDos : [ToDo] = []
-        
+    
+    var toDos : [ToDoCoreData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-     toDos = createToDos()
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        getToDos()
+        
+    }
+    
+    func getToDos() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            if let coreDataToDos = try? context.fetch(ToDoCoreData.fetchRequest()) as? [ToDoCoreData] {
+                if let theToDos = coreDataToDos{
+                    toDos = theToDos
+                    tableView.reloadData()
+                }
+            }
+        }
     }
     
     func createToDos() -> [ToDo] {
-    
+        
         let eggs = ToDo()
         eggs.name = "Buy Eggs"
         eggs.important = true
@@ -34,24 +49,26 @@ class ToDoTableViewController: UITableViewController {
         return [eggs, dog, cheese]
     }
     
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDos.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         let toDo = toDos[indexPath.row]
         
-        if toDo.important {
-           cell.textLabel?.text = "❗️" + toDo.name
-        } else {
-           cell.textLabel?.text = toDo.name
-
+        if let name = toDo.name {
+            
+            if toDo.important {
+                cell.textLabel?.text = "❗️" + name
+            } else {
+                cell.textLabel?.text = toDo.name
+                
+            }
         }
-        
         return cell
     }
     
@@ -62,17 +79,17 @@ class ToDoTableViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let addVC = segue.destination as? AddToDoViewController {
-        addVC.previousVC = self
+            addVC.previousVC = self
         }
         
         if let completeVC = segue.destination as? CompleteViewController {
             
             if let toDo = sender as? ToDo {
-            completeVC.selectedToDo = toDo
-            completeVC.previousVC = self
+                completeVC.selectedToDo = toDo
+                completeVC.previousVC = self
+            }
         }
+        
     }
     
-    }
-
 }
